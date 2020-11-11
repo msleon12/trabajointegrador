7f7ef3a7b2fd24f85d2328133ee419e688f2cf09
msleon12 <martinleon2013@gmail.com>
2020-11-11 15:43:11 -0300

    cambie cosas del html y arranqué con js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
-let url = 
-fetch()
+let url = "https://api.themoviedb.org/3/movie/552?api_key=12058c71aa3652a9d53642bacf937088"
+fetch(url)
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    console.log(data);
+})
+.catch(function(error){
+    console.log("El error fue " + error);
+})
