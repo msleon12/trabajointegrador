0f5518a65424a1d91c3699709a8b5c321ca8f2fa
msleon12 <martinleon2013@gmail.com>
2020-11-12 00:06:34 -0300

    avancé más con el buscador

--- js/app.js
+++ js/app.js
@@ -4,18 +4,24 @@ const url = "https://api.themoviedb.org/3/search/movie?api_key=12058c71aa3652a9d
 const image_url = "https://image.tmdb.org/t/p/w500"
 
 
-
+// Declaro otras variables
 const buttonElement = document.querySelector ("#search");
 const inputElement = document.querySelector ("#inputValue");
 const movieSearchable = document.querySelector ("#movies-searchable");
 
+// Función parte 1
 function movieSection(movies){
     return movies.map((movie) => {
-        return `
-        <img src=${image_url + movie.poster_path} data-movie-id=${movie.id}/>`;
-
+        if(movie.poster_path){
+            return `<img 
+                src=${image_url + movie.poster_path}
+                data-movie-id=${movie.id}
+                />`;
+        }
     })
 }
+
+// Función parte 2
 function createMovieContainer (movies){
     const movieElement = document.createElement("div");
     movieElement.setAttribute("class", "movie");
@@ -32,24 +38,36 @@ function createMovieContainer (movies){
     return movieElement;
 
 }
+
+function renderSearchMovies(data){
+    movieSearchable.innerHTML = "";
+    const movies = data.results;
+    const movieBlock = createMovieContainer(movies);
+    movieSearchable.appendChild(movieBlock);
+    console.log("data: ", data);
+}
+// Evento + Api para el buscador
 buttonElement.onclick = function(event){
     event.preventDefault();
     const value = inputElement.value
     
-    const newUrl = url + '&query=' + value
+    const newUrl = url + '&query=' + value;
     
     fetch(newUrl)
-        .then(function(response){
-            return response.json()
-        })
-        .then(function(data){
-            console.log("data: ", data);
-            const movies = data.results;
-            const movieBlock = createMovieContainer(movies);
-            movieSearchable.appendChild(movieBlock); 
-        })
-        .catch(function(error){
+        .then((res) => res.json())
+        .then(renderSearchMovies)
+        .catch((error) => {
             console.log("El error fue " + error);
         })
+    
+        inputElement.value ="";
     console.log("value: " + value);
+}
+
+// Event delegation. No se que es, pero esta bueno jajaja
+document.onclick = function(event){
+    const target = event.target;
+    if (target.tagName.toLowerCase() === "img"){
+        console.log('Hello');
+    }
 }
