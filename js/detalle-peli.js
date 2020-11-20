d3bfc0c3f58663adb33b3e0a7fd6551788659b75
msleon12 <martinleon2013@gmail.com>
2020-11-20 11:43:48 -0300

    l

--- js/detalle-peli.js
+++ js/detalle-peli.js
@@ -14,6 +14,8 @@ let id = queryObject.get('id');
 const api_key  = "12058c71aa3652a9d53642bacf937088"
 const urlDetalle = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}&language=es-ES` 
 const image_url = "https://image.tmdb.org/t/p/w500"
+const urlDetalleSerie = `https://api.themoviedb.org/3/tv/${id}?api_key=${api_key}&language=es-ES`
+
 
 // Pelicula principal
 fetch(urlDetalle)
